70e493c8ee811b66883fa005b306ab818b163c04
Patrick <wshoffner@ewu.edu>
2023-07-23 13:52:58 -0700

    Add project files.

---
+++ ClassManagerServer.Api/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClassManagerServer.Api.Controllers
+{
+    public class UserAuthenticationController : ControllerBase
+    {
+
+        private readonly ILogger<UserAuthenticationController> _logger;
+
+
+    }
+}

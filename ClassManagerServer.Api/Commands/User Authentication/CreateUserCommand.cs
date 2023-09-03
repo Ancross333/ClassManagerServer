f2acb5b830cf3a61805e4e41643f5c0f6a26ecd7
Patrick <wshoffner@ewu.edu>
2023-09-03 12:44:48 -0700

    Initialize Project

---
+++ ClassManagerServer.Api/Commands/User Authentication/CreateUserCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+using MediatR;
+
+namespace ClassManagerServer.Api.Commands.User_Authentication
+{
+    public class CreateUserCommand : IRequest<UserAuthenticationDto>
+    {
+
+        [DataMember]
+        public string Email { get; init; }
+        [DataMember] 
+        public string Password { get; init; }
+        [DataMember] 
+        public string FirstName { get; init; }
+        [DataMember]
+        public string LastName { get; init; }
+
+        public CreateUserCommand() { }
+
+        public CreateUserCommand(string email, string password, string firstName, string lastName)
+        {
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    public record UserAuthenticationDto
+    {
+
+    }
+}

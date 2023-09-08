8294024e496a0476ccc95185d3fd370ca0bac99f
Patrick <wshoffner@ewu.edu>
2023-09-07 18:32:39 -0700

    Cleanup Program.cs and remove manual validations

--- ClassManagerServer.Api/Controllers/UserAuthenticationController.cs
+++ ClassManagerServer.Api/Controllers/UserAuthenticationController.cs
@@ -15,15 +15,12 @@ namespace ClassManagerServer.Api.Controllers
     {
         private readonly IMediator _mediator;
         private readonly ILogger<UserAuthenticationController> _logger;
-        private readonly IValidator<CreateUserRequest> _validator;  // Step 1: Add a private field for the validator
 
         public UserAuthenticationController(ILogger<UserAuthenticationController> logger,
-                                             IMediator mediator,
-                                             IValidator<CreateUserRequest> validator)  // Step 2: Add an additional parameter for the validator
+                                             IMediator mediator)
         {
             _logger = logger;
             _mediator = mediator;
-            _validator = validator;  // Step 3: Assign the injected validator to the field
         }
 
         [HttpPost]
@@ -32,13 +29,6 @@ namespace ClassManagerServer.Api.Controllers
         public async Task<IActionResult> CreateUser([FromBody] CreateUserRequest request)
         {
             _logger.LogInformation("Request to create user has been made.");
-
-            // Step 4: Use the validator to validate the request
-            var validationResult = await _validator.ValidateAsync(request);
-            if (!validationResult.IsValid)
-            {
-                return BadRequest(validationResult.Errors);
-            }
 
             var cmd = new CreateUserCommand(request.Email, request.Password, request.FirstName, request.LastName);
             var data = await _mediator.Send(cmd);
--- ClassManagerServer.Api/Program.cs
+++ ClassManagerServer.Api/Program.cs
@@ -1,13 +1,5 @@
-using ClassManagerServer.Api.Commands.User_Authentication;
-using ClassManagerServer.Api.Controllers;
-using ClassManagerServer.Api.Requests;
-using ClassManagerServer.Api.Validations;
-using ClassManagerServer.EventHandlers;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using MediatR; // Make sure to add this for MediatR extensions.
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,11 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
-// Register MediatR and specify the assembly containing your command handlers.
-// Replace 'ClassManagerServer.EventHandlers' with the appropriate assembly if it's not correct.
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies( new[] { Assembly.GetExecutingAssembly() });
+    config.RegisterServicesFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
 });
 
 builder.Services.AddEndpointsApiExplorer();

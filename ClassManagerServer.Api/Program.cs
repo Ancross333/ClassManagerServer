10f5afc9c179148394dada65cf51995a49bf2c19
Patrick <wshoffner@ewu.edu>
2023-09-07 20:03:05 -0700

    Add database and local config

--- ClassManagerServer.Api/Program.cs
+++ ClassManagerServer.Api/Program.cs
@@ -1,5 +1,7 @@
+using ClassManagerServer.Db;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,9 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
 });
+
+builder.Services.AddDbContext<ClassManagerDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ ClassManagerServer.Db/ClassManagerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClassManagerServer.Db
+{
+    public class ClassManagerDbContext : DbContext
+    {
+        public ClassManagerDbContext(DbContextOptions<ClassManagerDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}

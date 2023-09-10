e8180ff80ac0b92ff624c2a7a54f2f1fd76d280d
Patrick <wshoffner@ewu.edu>
2023-09-10 11:33:14 -0700

    Add validator for CreateUserRequest

---
+++ ClassManagerServer.Api.Tests/Validators/CreateUserRequestValidatorTests.cs
@@ -0,0 +1,207 @@
+﻿using ClassManagerServer.Api.Requests;
+using ClassManagerServer.Api.Validations;
+using ClassManagerServer.Domain.Enums;
+using FluentAssertions;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace ClassManagerServer.Api.Tests.Validators
+{
+    public class CreateUserRequestValidatorTests
+    {
+        private readonly AutoMocker _mocker;
+
+        public CreateUserRequestValidatorTests()
+        {
+            _mocker = new AutoMocker();
+        }
+
+        [Fact]
+        public void It_rejects_invalid_emails()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Invalid Email",
+                Password = "ValidPassword1",
+                FirstName = "First Name",
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_short_passwords()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "short",
+                FirstName = "First Name",
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_numberless_passwords()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "NoNumbers",
+                FirstName = "First Name",
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_no_lowercase_passwords()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "ALLCAPS2001",
+                FirstName = "First Name",
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_no_cap_passwords()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "no_caps2001",
+                FirstName = "First Name",
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_short_first_name()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "ValidPassword1",
+                FirstName = "F",
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_long_first_name()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "ValidPassword1",
+                FirstName = new string('a', 51),
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_short_last_name()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "ValidPassword1",
+                FirstName = "First Name",
+                LastName = "L",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_long_last_name()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "ValidPassword1",
+                FirstName = "First Name",
+                LastName = new string('b', 51),
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_rejects_invalid_user_type()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "ValidPassword1",
+                FirstName = "First Name",
+                LastName = "LastName",
+                UserType = (UserType)999
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(false);
+        }
+
+        [Fact]
+        public void It_accepts_valid_requests()
+        {
+            var validator = new CreateUserRequestValidator();
+            var request = new CreateUserRequest()
+            {
+                Email = "Valid@Email.net",
+                Password = "ValidPassword1",
+                FirstName = "First Name",
+                LastName = "LastName",
+                UserType = UserType.Student
+            };
+
+            var result = validator.Validate(request);
+            result.IsValid.Should().Be(true);
+        }
+    }
+}
--- ClassManagerServer.Api/Validations/CreateUserRequestValidator.cs
+++ ClassManagerServer.Api/Validations/CreateUserRequestValidator.cs
@@ -1,5 +1,7 @@
 ﻿using ClassManagerServer.Api.Requests;
+using ClassManagerServer.Domain.Enums;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace ClassManagerServer.Api.Validations
 {
@@ -8,8 +10,75 @@ namespace ClassManagerServer.Api.Validations
 
         public CreateUserRequestValidator() 
         {
-            RuleFor(x => x.FirstName).NotEmpty()
-                .WithMessage("Validator Is Working (Req)");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage(x => $"Field {nameof(x.Email)} cannot be empty")
+                .Must(BeAValidEmailAddress)
+                .WithMessage("Email address is invalid");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage(x => $"Field {nameof(x.Password)} cannot be empty")
+                .Must(HaveAtLeastEightCharacters)
+                .WithMessage(x => $"Field {nameof(x.Password)} must have at least 8 characters")
+                .Must(HaveAtLeastOneUpperCaseLetter)
+                .WithMessage(x => $"Field {nameof(x.Password)} must have at least 1 uppercase letter")
+                .Must(HaveAtLeastOneLowerCaseLetter)
+                .WithMessage(x => $"Field {nameof(x.Password)} must have at least 1 lowercase letter")
+                .Must(HaveAtLeastOneDigit)
+                .WithMessage(x => $"Field {nameof(x.Password)} must have at least 1 digit");
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage(x => $"Field {nameof(x.FirstName)} cannot be empty")
+                .MinimumLength(2).WithMessage("First name must be at least 2 characters long")
+                .MaximumLength(50).WithMessage("First name must be less than 50 characters long")
+                .Matches("^[a-zA-Z-'\\s]+$").WithMessage("First name can only contain letters, hyphens, apostrophes or spaces");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage(x => $"Field {nameof(x.LastName)} cannot be empty")
+                .MinimumLength(2).WithMessage("Last name must be at least 2 characters long")
+                .MaximumLength(50).WithMessage("Last name must be less than 50 characters long")
+                .Matches("^[a-zA-Z-'\\s]+$").WithMessage("Last name can only contain letters, hyphens, apostrophes or spaces");
+
+            RuleFor(x => x.UserType)
+                .Must(BeAValidUserType)
+                .WithMessage(x => $"Field {nameof(x.UserType)} Is not a valid user type");
+                
         }
+
+        private static bool BeAValidEmailAddress(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            string emailRegex = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailRegex);
+        }
+
+        private static bool HaveAtLeastEightCharacters(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Length >= 8;
+        }
+
+        private static bool HaveAtLeastOneUpperCaseLetter(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Any(char.IsUpper);
+        }
+
+        private static bool HaveAtLeastOneLowerCaseLetter(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Any(char.IsLower);
+        }
+
+        private static bool HaveAtLeastOneDigit(string password)
+        {
+            return !string.IsNullOrEmpty(password) && password.Any(char.IsDigit);
+        }
+        public static bool BeAValidUserType(UserType userType)
+        {
+            return Enum.IsDefined(typeof(UserType), userType);
+        }
+
+
     }
 }
